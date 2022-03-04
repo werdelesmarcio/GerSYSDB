c5cf8f3099544d22b504c56b0e62c13d483aec44
Werdeles Marcio de C. Soares <36682515+werdelesmarcio@users.noreply.github.com>
2022-03-04 00:50:24 -0300

    Adicionar arquivos de projeto.

---
+++ GERSYSDB/tb_datausers.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tb_datausers]
+(
+	[id_datausers] INT NOT NULL, 
+    [addres_datausers] VARCHAR(255) NOT NULL, 
+    [contact_datausers] VARCHAR(100) NOT NULL, 
+    CONSTRAINT [FK_tb_datausers_tb_users] FOREIGN KEY ([id_datausers]) REFERENCES [tb_users]([id_user]) 
+)
---
+++ GERSYSDB/tb_users.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tb_users]
+(
+	[id_user] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [name_user] NCHAR(10) NOT NULL, 
+    [active_user] BIT NOT NULL
+)
